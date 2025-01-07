35bbc1e5decbe29845b11cb579d5db70eb8d6a3c
Julie Rose <julie.rose@noaa.gov>
2025-01-07 17:39:52 -0500

    Automate file download
    Automate expedition-level download of dive summary text files from individual NCEI dive landing pages. Need to add code to accommodate UCH dives - these do not have dive summary text files and will throw an error mid-loop.

---
+++ Dive_summary_file_download.R
@@ -0,0 +1,36 @@
+#Automating dive summary text file downloads
+#Using EX2014 as a test case
+
+dive_names <- c("DIVE01", "DIVE02", "DIVE03", "DIVE04", "DIVE05", "DIVE06", "DIVE07", "DIVE08", "DIVE09", "DIVE10", "DIVE11")
+data_name_lower <- tolower(data_name)
+
+for(i in dive_names){
+  
+url <- paste0("https://oer.hpc.msstate.edu/okeanos/",data_name_lower,"/", data_name_lower,"-",i,"-ancillary-data.zip")
+
+download.file(url, destfile = paste0(wd,"/dive_summaries/download_test/",i,"-ancillary-data.zip"))
+
+zip_file_paths <- unzip(paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",i,"-ancillary-data.zip"),
+                        exdir = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/"),
+                        list = TRUE)
+
+unzip(paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",i,"-ancillary-data.zip"),
+      exdir = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/"),
+      files = zip_file_paths[2,1])
+
+file.rename(from = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",zip_file_paths[2,1]),
+            to = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",i,".txt"))
+            
+unlink(x = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",i,"-ancillary-data.zip"), recursive = TRUE)
+unlink(x = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",zip_file_paths[1,1]), recursive = TRUE)
+}
+
+#test code to identify UCH dives (these do not have dive summaries), print a warning, and not interrupt the loop of a full-expedition download
+UCH_url <- "https://oer.hpc.msstate.edu/okeanos/ex2103/ex2103-DIVE11-ancillary-data.zip"
+
+suppressWarnings(UCH_test <- try(download.file(UCH_url, destfile = paste0(wd,"/dive_summaries/download_test/","DIVE11-ancillary-data.zip")), silent = TRUE))
+if(inherits(UCH_test, "try-error")){
+  print("No Dive summary file")
+}
+
+
