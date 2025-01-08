68ae1eb7508cf347be43711850be4f5d9571c6ef
Julie Rose <julie.rose@noaa.gov>
2025-01-08 17:03:50 -0500

    Create dive summary text file directory
    And update filepaths

--- Dive_summary_file_download.R
+++ Dive_summary_file_download.R
@@ -3,26 +3,27 @@
 
 dive_names <- c("DIVE01", "DIVE02", "DIVE03", "DIVE04", "DIVE05", "DIVE06", "DIVE07", "DIVE08", "DIVE09", "DIVE10", "DIVE11")
 data_name_lower <- tolower(data_name)
+dir.create(paste0(wd,"/dive_summaries/"))
 
 for(i in dive_names){
   
 url <- paste0("https://oer.hpc.msstate.edu/okeanos/",data_name_lower,"/", data_name_lower,"-",i,"-ancillary-data.zip")
 
-download.file(url, destfile = paste0(wd,"/dive_summaries/download_test/",i,"-ancillary-data.zip"))
+download.file(url, destfile = paste0(wd,"/dive_summaries/",i,"-ancillary-data.zip"))
 
-zip_file_paths <- unzip(paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",i,"-ancillary-data.zip"),
-                        exdir = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/"),
+zip_file_paths <- unzip(paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/",i,"-ancillary-data.zip"),
+                        exdir = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/"),
                         list = TRUE)
 
-unzip(paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",i,"-ancillary-data.zip"),
-      exdir = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/"),
+unzip(paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/",i,"-ancillary-data.zip"),
+      exdir = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/"),
       files = zip_file_paths[2,1])
 
-file.rename(from = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",zip_file_paths[2,1]),
-            to = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",i,".txt"))
+file.rename(from = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/",zip_file_paths[2,1]),
+            to = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/",i,".txt"))
             
-unlink(x = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",i,"-ancillary-data.zip"), recursive = TRUE)
-unlink(x = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/download_test/",zip_file_paths[1,1]), recursive = TRUE)
+unlink(x = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/",i,"-ancillary-data.zip"), recursive = TRUE)
+unlink(x = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/",zip_file_paths[1,1]), recursive = TRUE)
 }
 
 #test code to identify UCH dives (these do not have dive summaries), print a warning, and not interrupt the loop of a full-expedition download
