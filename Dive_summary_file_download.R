319d261c9cab4790f744bcac61b294621d4c0cea
Julie Rose <julie.rose@noaa.gov>
2025-01-10 11:14:15 -0500

    Update dive summary file download
    Added missing file check to dive summary download - UCH dives have no dive summary text files due to data restrictions

--- Dive_summary_file_download.R
+++ Dive_summary_file_download.R
@@ -1,7 +1,9 @@
-#Automating dive summary text file downloads
-#Using EX2014 as a test case
+#Automating dive summary text file downloads, using EX2107 as a test case because
+#there is a UCH dive in the middle of the expedition
 
-dive_names <- c("DIVE01", "DIVE02", "DIVE03", "DIVE04", "DIVE05", "DIVE06", "DIVE07", "DIVE08", "DIVE09", "DIVE10", "DIVE11")
+wd <- "C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/EX2107"
+data_name <- "EX2107"
+dive_names <- c("DIVE03", "DIVE04", "DIVE05", "DIVE06", "DIVE07", "DIVE08", "DIVE09", "DIVE10", "DIVE11", "DIVE12", "DIVE13", "DIVE14")
 data_name_lower <- tolower(data_name)
 dir.create(paste0(wd,"/dive_summaries/"))
 
@@ -9,8 +11,17 @@ for(i in dive_names){
   
 url <- paste0("https://oer.hpc.msstate.edu/okeanos/",data_name_lower,"/", data_name_lower,"-",i,"-ancillary-data.zip")
 
-download.file(url, destfile = paste0(wd,"/dive_summaries/",i,"-ancillary-data.zip"))
+suppressWarnings(UCH_test <- try(download.file(url, destfile = paste0(wd,"/dive_summaries/",i, "-ancillary-data.zip")), silent = TRUE))
+if(inherits(UCH_test, "try-error")){
+  print(paste0("No ", i," Dive summary file"))
+}
+}
 
+#stop here and see if there are any missing dive summaries based on the output
+#of the above code; update dive_names if needed or else the code below will be
+#interrupted by a missing zip folder
+
+for(i in dive_names){
 zip_file_paths <- unzip(paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/",i,"-ancillary-data.zip"),
                         exdir = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/"),
                         list = TRUE)
@@ -24,14 +35,6 @@ file.rename(from = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expe
             
 unlink(x = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/",i,"-ancillary-data.zip"), recursive = TRUE)
 unlink(x = paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/",data_name,"/dive_summaries/",zip_file_paths[1,1]), recursive = TRUE)
-}
-
-#test code to identify UCH dives (these do not have dive summaries), print a warning, and not interrupt the loop of a full-expedition download
-UCH_url <- "https://oer.hpc.msstate.edu/okeanos/ex2103/ex2103-DIVE11-ancillary-data.zip"
-
-suppressWarnings(UCH_test <- try(download.file(UCH_url, destfile = paste0(wd,"/dive_summaries/download_test/","DIVE11-ancillary-data.zip")), silent = TRUE))
-if(inherits(UCH_test, "try-error")){
-  print("No Dive summary file")
 }
 
 
