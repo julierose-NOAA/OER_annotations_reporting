e7cfa4edc0157b5f82f5467711aba35f346d1a55
Julie Rose <julie.rose@noaa.gov>
2025-04-14 15:30:45 -0400

    ROV_track import & clean
    Automates the import of a group of ROV track .csv files into a single data frame. Also automates cleaning those files and extracting just the benthic portion of each dive.

--- ROV_track_processing.R
+++ ROV_track_processing.R
@@ -2,23 +2,78 @@
 #calculating ROV distance traveled per dive
 #note all OER coordinates are in WGS84
 
-#develop functioning workflow for single dive before creating function
-ROV_test_import <- read.csv("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/EX1803/ROV_tracks/EX1803_DIVE03_ROVtrack.csv")
+#-------------------------------------------------------------------------------
+#NEW FUNCTIONS
+#move these to functions folder and add source() when this work is finished
 
-#eventually pull this from benthic_times dataframe for multiple dives
-ROV_benthic_start_time <- as.POSIXct("2018-04-16 14:56:48", tz = "UTC")
-ROV_benthic_end_time <- as.POSIXct("2018-04-16 21:36:19", tz = "UTC")
 
-ROV_test <- ROV_test_import |> 
-  dplyr::select("UNIXTIME","DEPTH","ALT","LAT_DD","LON_DD") |> 
-  dplyr::rename(unix_time = "UNIXTIME", depth_m = "DEPTH", altitude_m = "ALT",
-                latitude_dd = "LAT_DD", longitude_dd = "LON_DD") |> 
-  dplyr::mutate(UTC = lubridate::as_datetime(unix_time)) |>
-  dplyr::filter(UTC>=ROV_benthic_start_time & UTC<=ROV_benthic_end_time)
+#reads in .csv file and stores the filename in a column, for use in 
+#automating extraction of dive number in the ROV_clean function
+ROV_import <- function(filename){
+  ROV <- read.csv(filename, header = TRUE)
+  ROV$dive_number <- filename
+  ROV
+}
 
-#eventually add expedition and dive number
+#adds expedition name, extracts dive number, selects relevant columns, converts
+#unixtime to UTC
+ROV_clean <- function(x){
+  x |> 
+    dplyr::mutate(expedition = data_name) |> 
+    dplyr::select(expedition, dive_number, "UNIXTIME","DEPTH","ALT","LAT_DD","LON_DD") |> 
+    dplyr::rename(unix_time = "UNIXTIME", depth_m = "DEPTH", altitude_m = "ALT",
+                  latitude_dd = "LAT_DD", longitude_dd = "LON_DD") |>
+    dplyr::mutate(UTC = lubridate::as_datetime(unix_time)) |> 
+    dplyr::mutate(across(dive_number, \(x) stringr::str_extract(x, "DIVE(\\d{2})"))) |> 
+    dplyr::mutate(across(dive_number, \(x) stringr::str_replace(x, "DIVE",""))) |> 
+    dplyr::mutate(dive_number = as.numeric(dive_number))
+}
+#-------------------------------------------------------------------------------
+#creates vector of filenames in the ROV_tracks folder
+ROV_paths <- list.files(paste0(wd, "/ROV_tracks"),
+                        pattern = "[.]csv$", full.names = TRUE)
 
-View(ROV_test)
+#for each file in the ROV_paths vector, applies import function - creates a 
+#list where each element is the ROV track data for a single dive
+ROV_import_list <- purrr::map(ROV_paths, \(x) ROV_import(x))
+
+#applies cleaning function to each element in the list and combines the list
+#elements together by row into a data frame with all dives
+ROV_import_clean <- ROV_import_list |> 
+  purrr::map(ROV_clean) |> 
+  purrr::list_rbind()
+
+View(ROV_import_clean)
+
+#Joins the ROV track data frame with the benthic times data frame to facilitate
+#extracting the benthic portion of each dive
+ROV_join <- dplyr::left_join(ROV_import_clean, benthic_times,
+                   dplyr::join_by("dive_number" == "dive_number"))
+
+str(ROV_join)
+
+#extracts benthic part of each dive
+ROV_benthic <- ROV_join |> 
+  dplyr::group_by(dive_number) |> 
+  dplyr::filter(UTC>=benthic_start & UTC<=benthic_end) |> 
+  dplyr::ungroup()
+
+# ROV_test_import <- read.csv("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/EX1803/ROV_tracks/EX1803_DIVE03_ROVtrack.csv")
+# 
+# #eventually pull this from benthic_times dataframe for multiple dives
+# ROV_benthic_start_time <- as.POSIXct("2018-04-16 14:56:48", tz = "UTC")
+# ROV_benthic_end_time <- as.POSIXct("2018-04-16 21:36:19", tz = "UTC")
+# 
+# ROV_test <- ROV_test_import |> 
+#   dplyr::select("UNIXTIME","DEPTH","ALT","LAT_DD","LON_DD") |> 
+#   dplyr::rename(unix_time = "UNIXTIME", depth_m = "DEPTH", altitude_m = "ALT",
+#                 latitude_dd = "LAT_DD", longitude_dd = "LON_DD") |> 
+#   dplyr::mutate(UTC = lubridate::as_datetime(unix_time)) |>
+#   dplyr::filter(UTC>=ROV_benthic_start_time & UTC<=ROV_benthic_end_time)
+# 
+# #eventually add expedition and dive number
+# 
+# View(ROV_test)
 
 #-------------------------------------------------------------------------------
 #smoothing options for lat/long because the high resolution data can be messy
