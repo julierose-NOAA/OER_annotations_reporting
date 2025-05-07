6f0bbf098e14001b49dbec7b6bc253749cea885e
Julie Rose <julie.rose@noaa.gov>
2025-05-07 09:45:54 -0400

    ROV distance function
    Calculates 3D distance traveled between pairs of rows within the ROV track data frame and saves as new column

---
+++ Functions/ROV_distance.R
@@ -0,0 +1,10 @@
+#Calculates 3D distance traveled between pairs of rows within the ROV track
+#data frame and saves as new column
+
+ROV_distance <- function(data, lat, long){
+  data |> 
+    dplyr::mutate(Haversine = c(distHaversine(cbind({{long}},{{lat}})),0),
+                  depth_distance_m = c(diff(altitude_m),0),
+                  distance_3D_m = sqrt((depth_distance_m^2) + (Haversine^2)))
+  
+}
