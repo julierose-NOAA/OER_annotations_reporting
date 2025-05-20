04dc8670b5453057edfca6f99e4028e5d3076b13
Julie Rose <161734285+julierose-NOAA@users.noreply.github.com>
2025-05-20 17:32:02 -0400

    Update ROV distance traveled
    1. Loop code so that it does the whole expedition
    2. Comment out the plots
    3. Update outlier threshold to 90th quantile because the max value was unstable in the noisy data.
    4. Started work on automatically generating dive names from file names

--- ROV_distance_traveled.R
+++ ROV_distance_traveled.R
@@ -19,10 +19,10 @@ lapply(function_names, source)
 #set file paths and data names that correspond to file names
 ROV_filepath <- "C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/EX2104/ROV_tracks/"
 expedition <- "EX2104"
-dive_name <- "EX2104_DIVE01"
 
 #location of the benthic times data frame that is an output of the 
-#Benthic_annotations_cleaning script
+#Benthic_annotations_cleaning script. This contains start/end times for all dives
+#in an expedition so only needs to be imported once.
 benthic_times_wd <- paste0("C:/Users/julie.rose/Documents/1-OER/Biodiversity/expeditions/", 
                            expedition, "/exports/")
 
@@ -33,9 +33,27 @@ benthic_times$benthic_start <- lubridate::ymd_hms(benthic_times$benthic_start)
 benthic_times$benthic_end <- lubridate::ymd_hms(benthic_times$benthic_end)
 str(benthic_times) #check to make sure these are the right times for your expedition
 
+ROV_distance_traveled_vec <- c() #this will become the vector of distances
 #-------------------------------------------------------------------------------
+#this section needs cleaning up; rename function and move it to new script in  functions
+#folder
+ROV_dive_numbers <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19") #automate extracting this from file names
+
+ROV_name_list <- list.files(path = ROV_filepath, pattern = "[.]csv$", full.names = TRUE)
+test <- function(x) {
+  name <- stringr::str_extract(x, "DIVE(\\d{2})") 
+  stringr::str_replace(name, "DIVE","")
+}
+ROV_name_list <- sapply(ROV_name_list, test, USE.NAMES = FALSE)
+
+#-------------------------------------------------------------------------------
+#the remaining code runs as a loop processing each dive and creating a dataframe
+#please note processing time is approximately 8 minutes PER DIVE on a standard
+#laptop
+
+for(i in ROV_dive_numbers){
 #import
-ROV_import_df <- ROV_import(paste0(ROV_filepath,dive_name,"_ROVtrack.csv"))
+ROV_import_df <- ROV_import(paste0(ROV_filepath,expedition,"_DIVE",i,"_ROVtrack.csv"))
 
 #clean
 ROV_clean_df <- ROV_clean(ROV_import_df)
@@ -56,15 +74,17 @@ ROV_benthic <- ROV_join |>
 ROV_SMA_window <- seq(from = 1, to = nrow(ROV_benthic), by = 100)
 ROV_SMA_distance <- c()
 
-for(i in ROV_SMA_window){
+for(j in ROV_SMA_window){
   ROV_smooth <- ROV_benthic |> 
-    dplyr::mutate(Lat_SMA = TTR::SMA(latitude_dd, n = i),
-                  Lon_SMA = TTR::SMA(longitude_dd, n = i),
-                  Depth_SMA = TTR::SMA(depth_m, n = i))
+    dplyr::mutate(Lat_SMA = TTR::SMA(latitude_dd, n = j),
+                  Lon_SMA = TTR::SMA(longitude_dd, n = j),
+                  Depth_SMA = TTR::SMA(depth_m, n = j))
   ROV_distance_smooth <- ROV_distance(ROV_smooth, lat = Lat_SMA, long = Lon_SMA)
   ROV_distance_m <- sum(ROV_distance_smooth$distance_3D_m, na.rm = TRUE)
   ROV_SMA_distance <- c(ROV_SMA_distance, ROV_distance_m)
 }
+
+#add print indicating which dive is being iterated
 
 #create data frame of smoothing window and total ROV distance traveled
 ROV_SMA_df <- as.data.frame(cbind(ROV_SMA_window, ROV_SMA_distance))
@@ -84,41 +104,53 @@ ROV_SMA_df_outliers <- as.data.frame(MadMed_out_dist[[3]])
 colnames(ROV_SMA_df_outliers) = c("distance")
 summary(ROV_SMA_df_outliers) #visual check
 
-outlier_threshold <- max(ROV_SMA_df_outliers$distance) #use max b/c values are negative
+outlier_threshold <- quantile(ROV_SMA_df_outliers$distance, probs = 0.9) #use 90th percentile b/c values are negative
 ROV_threshold <- ROV_SMA_df[which(ROV_SMA_df$Distance_diff > outlier_threshold, arr.ind = TRUE)[1],]
 
 ROV_distance_traveled <- ROV_threshold$ROV_SMA_distance
 
+ROV_distance_traveled_vec <- c(ROV_distance_traveled_vec, ROV_distance_traveled)
+
+print(paste0("Dive",i," completed"))
+}
+
+ROV_distance_df <- data.frame(expedition = expedition, 
+                              dive_number = as.numeric(ROV_dive_numbers), 
+                              distance_m = ROV_distance_traveled_vec)
+
+write.csv(ROV_distance_df, paste0(wd,"/exports/", expedition,"_ROV_distance.csv"),
+          row.names = FALSE)
+
 #-------------------------------------------------------------------------------
 #Visualize outlier detection results
 
-ggplot2::ggplot(ROV_SMA_df, ggplot2::aes(x = ROV_SMA_window, y = ROV_SMA_distance)) +
-  ggplot2::geom_point() +
-  ggplot2::labs(x = "Number of ROV position points used in simple moving average smooth", 
-       y = "ROV distance traveled (m)",
-       title = "Change in predicted ROV distance traveled with increased smoothing",
-       subtitle = dive_name) +
-  ggplot2::geom_vline(xintercept = ROV_distance_traveled, color = "#FF6C57", linewidth = 1.5) +
-  ggplot2::theme_bw()
+# ggplot2::ggplot(ROV_SMA_df, ggplot2::aes(x = ROV_SMA_window, y = ROV_SMA_distance)) +
+#   ggplot2::geom_point() +
+#   ggplot2::labs(x = "Number of ROV position points used in simple moving average smooth", 
+#        y = "ROV distance traveled (m)",
+#        title = "Change in predicted ROV distance traveled with increased smoothing",
+#        subtitle = dive_name) +
+#   ggplot2::geom_vline(xintercept = ROV_distance_traveled, color = "#FF6C57", linewidth = 1.5) +
+#   ggplot2::theme_bw()
 
 #------------------------------------------------------------------------------
 #Visualize raw and smoothed track lines
 
-ROV_smooth_predicted <- ROV_benthic |> 
-  dplyr::mutate(Lat_SMA = TTR::SMA(latitude_dd, n = ROV_threshold$ROV_SMA_window),
-                Lon_SMA = TTR::SMA(longitude_dd, n = ROV_threshold$ROV_SMA_window),
-                Depth_SMA = TTR::SMA(depth_m, n = ROV_threshold$ROV_SMA_window))
-
-#raw data
-ROV_benthic |>
-  leaflet::leaflet() |> 
-  leaflet::addTiles() |>  
-  leaflet::addPolylines(lng = ~longitude_dd, lat = ~latitude_dd)
-
-#smoothed data
-ROV_smooth_predicted |>
-  leaflet::leaflet() |> 
-  leaflet::addTiles() |>  
-  leaflet::addPolylines(lng = ~Lon_SMA, lat = ~Lat_SMA)
+# ROV_smooth_predicted <- ROV_benthic |> 
+#   dplyr::mutate(Lat_SMA = TTR::SMA(latitude_dd, n = ROV_threshold$ROV_SMA_window),
+#                 Lon_SMA = TTR::SMA(longitude_dd, n = ROV_threshold$ROV_SMA_window),
+#                 Depth_SMA = TTR::SMA(depth_m, n = ROV_threshold$ROV_SMA_window))
+# 
+# #raw data
+# ROV_benthic |>
+#   leaflet::leaflet() |> 
+#   leaflet::addTiles() |>  
+#   leaflet::addPolylines(lng = ~longitude_dd, lat = ~latitude_dd)
+# 
+# #smoothed data
+# ROV_smooth_predicted |>
+#   leaflet::leaflet() |> 
+#   leaflet::addTiles() |>  
+#   leaflet::addPolylines(lng = ~Lon_SMA, lat = ~Lat_SMA)
 
 
