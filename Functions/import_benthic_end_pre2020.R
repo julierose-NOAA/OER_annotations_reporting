dc932d7dfb9174a2b0fb32adba7df633d2ec6b5d
Julie Rose <161734285+julierose-NOAA@users.noreply.github.com>
2025-05-22 14:49:18 -0400

    Bug fix

--- Functions/import_benthic_end_pre2020.R
+++ Functions/import_benthic_end_pre2020.R
@@ -3,10 +3,10 @@
 
 import_benthic_end_pre2020 <- function(filename) {
   dive_summary <- scan(filename, what = 'character', skip = 2, sep="\t")
-  if(dive_summary[24] == "N/A") {
+  if(dive_summary[17] == "N/A") {
     print("Warning: Not a benthic dive")
   }
-  else (end_benthic <- as.POSIXct(dive_summary[24], tz="UTC", 
+  else (end_benthic <- as.POSIXct(dive_summary[17], tz="UTC", 
                             format = "%Y-%m-%dT%H:%M:%OS")
   )
 }
